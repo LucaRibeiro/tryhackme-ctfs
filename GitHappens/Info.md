01f27e137112b2975efba58ba4510d10d5645d71
Luca Ribeiro <luca.rsr.luca@gmail.com>
2021-05-12 07:00:20 -0300

    GitHappens

---
+++ GitHappens/Info.md
@@ -0,0 +1,8 @@
+# IP
+10.10.120.226
+
+# Portas
+80/tcp open  http    syn-ack ttl 61 nginx 1.14.0 (Ubuntu)
+
+# Vuln
+Git exposure
---
+++ GitHappens/script.js
@@ -0,0 +1,50 @@
+const _0x4368 = ['+(\x20+[^', '471197', 'value', 'RegExp', 'functi', 'test', 'CbRnH', 'passwo', 'userna', 'TML', 'tml', 'a865c5', '+[^\x20]}', 'a5f298', 'cookie', 'admin', '3a71fd', 'getEle', 'login-', '^([^\x20]', 'TEhxP', 'href', 'f64cb3', '51a151', 'd84319', 'D\x20USER', 'digest', 'R\x20PASS', 'oard.h', 'error', '\x20]+)+)', '19a3c0', 'f80f67', '/dashb', 'bea070', '3ec9cb', 'padSta', 'from', '4004c2', 'WORD!', 'map', 'NAME\x20O', 'encode', 'INVALI', 'a5106e', 'baf89f', '6a7c7c', 'elemen', '9a88db', 'log', 'join', 'innerH', 'SaltyB', 'apply', 'ned', '442a9d', 'mentBy'];
+(function(_0x1ef2d8, _0x436806) {
+    const _0x2c2818 = function(_0x302738) {
+            while (--_0x302738) {
+                _0x1ef2d8['push'](_0x1ef2d8['shift']());
+            }
+        },
+        _0x6f8b4a = function() {
+            const _0x2e9681 = {
+                    'data': {
+                        'key': 'cookie',
+                        'value': 'timeout'
+                    },
+                    'setCookie': function(_0x329b53, _0x28dc3d, _0x22f4a3, _0x6012c1) {
+                        _0x6012c1 = _0x6012c1 || {};
+                        let _0x3d8f23 = _0x28dc3d + '=' + _0x22f4a3,
+                            _0x18026e = 0x0;
+                        for (let _0x4175c9 = 0x0, _0x25d1be = _0x329b53['length']; _0x4175c9 < _0x25d1be; _0x4175c9++) {
+                            const _0x109e81 = _0x329b53[_0x4175c9];
+                            _0x3d8f23 += ';\x20' + _0x109e81;
+                            const _0x1e9a27 = _0x329b53[_0x109e81];
+                            _0x329b53['push'](_0x1e9a27), _0x25d1be = _0x329b53['length'], _0x1e9a27 !== !![] && (_0x3d8f23 += '=' + _0x1e9a27);
+                        }
+                        _0x6012c1['cookie'] = _0x3d8f23;
+                    },
+                    'removeCookie': function() {
+                        return 'dev';
+                    },
+                    'getCookie': function(_0x3e797a, _0x2a5b7d) {
+                        _0x3e797a = _0x3e797a || function(_0x242cdf) {
+                            return _0x242cdf;
+                        };
+                        const _0x996bc1 = _0x3e797a(new RegExp('(?:^|;\x20)' + _0x2a5b7d['replace'](/([.$?*|{}()[]\/+^])/g, '$1') + '=([^;]*)')),
+                            _0x51d0ee = function(_0x439650, _0x52fa41) {
+                                _0x439650(++_0x52fa41);
+                            };
+                        return _0x51d0ee(_0x2c2818, _0x436806), _0x996bc1 ? decodeURIComponent(_0x996bc1[0x1]) : undefined;
+                    }
+                },
+                _0x17997b = function() {
+                    const _0x383e88 = new RegExp('\x5cw+\x20*\x5c(\x5c)\x20*{\x5cw+\x20*[\x27|\x22].+[\x27|\x22];?\x20*}');
+                    return _0x383e88['test'](_0x2e9681['removeCookie']['toString']());
+                };
+            _0x2e9681['updateCookie'] = _0x17997b;
+            let _0x39ee22 = '';
+            const _0xad377 = _0x2e9681['updateCookie']();
+            if (!_0xad377) _0x2e9681['setCookie'](['*'], 'counter', 0x1);
+            else _0xad377 ? _0x39ee22 = _0x2e9681['getCookie'](null, 'counter') : _0x2e9681['removeCookie']();
+        };
+    _0x6f8b4a();
